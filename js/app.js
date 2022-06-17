16321382b4cbfcad6f3101f41449ea976381b955
juanir33 <guaca33@gmail.com>
2022-06-17 07:50:55 -0300

    Creo funcion, agregar tarea, validar formulario.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const axiosClient = axios.create({
     baseURL: 'http://localhost:8080/api'
 })
 const containerCards = selector('.tarea_mostrar_cards');
-//let tareas = [];
+
 
 
 
@@ -24,6 +24,7 @@ const getAll = async ()=>{
                    </button>
                    <input
                      type="text"
+                     id="nuevaSub"
                      class="form-control"
                      placeholder="Nueva tarea"
                      aria-label="Nueva tarea"
@@ -51,9 +52,54 @@ const getAll = async ()=>{
            console.log(item.subtareas)
         })
 
-    //console.log(data);
+  
 }
 getAll()
 
+const crearNuevaTarea= async ()=>{
+  
+  let title = selector("#NuevaTarea").value;
+  let body = {};
+  let error = validate(title);
+  if(error === ""){  
+   body = {
+    "titulo": title,
+  }
+  try{
+    let response = await axiosClient.post('/tarea', body);
+  }catch(e){
+    printError(e.message);
+  }}else{
+    printError(error)
+
+  }
+
+ 
+}
 
 
+const crearNuevaSub = async (id)=>{
+    let inputSub = selector("#nuevaSub");.
+    
+}
+
+const printError = (err) => {
+  let alerta= selector('#alert');
+  alerta.innerText = err;
+  alerta.classList.remove('d-none');
+  setTimeout(function(){
+    alerta.classList.add('d-none')
+  }, 5000);
+}
+const validate = (value)=>{
+  let error = "";
+  
+   if(value.length <= 2 ){
+     error = "Ingresa mas de 2 letras";
+   }else if(!/^[a-zA-Z0-9]{2,}$/i.test(value.split(" ").join(""))){
+    error = "Solo se permiten letras y numeros"
+   }else if(value.length > 30){
+    error = "Solo se permite hasta 30 caracteres"
+   }
+   return error
+}
